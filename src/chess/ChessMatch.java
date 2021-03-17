9b99f0ea8908eddb370ef63c8e6c3c6a5d726c00
PinkPynchon <olsf21@gmail.com>
2021-03-17 10:09:33 -0300

    ChessExcpetion and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,10 +24,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public void placeChessPiece(char column, int row, ChessPiece chessPiece) {
+		board.placePiece(chessPiece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeChessPiece('b', 6, new Rook(board, Color.WHITE));
+		placeChessPiece('e', 8, new King(board, Color.BLACK));
+		placeChessPiece('e', 1,  new King(board, Color.WHITE));
 	}
 
 }
