7f959802d1b3dde451a08bbc48ff456816bd1d60
PinkPynchon <olsf21@gmail.com>
2021-03-18 15:23:12 -0300

    Pawn

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,79 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+
+	public Pawn(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		if (getColor() == Color.WHITE) {
+			p.setValues(position.getRow(), position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			/*
+			 * Cria duas posicoes auxiliares, p e p2. Testa se é possível mover duas casas para frente usando a posicão inicial
+			 * e testa se há alguma peca que possa impedir que o peao se mova duas casas. Por exemplo, no caso em que o peão
+			 * não tenha se movido ainda, mas há um outro peão diretamente na sua frente. 
+			*/
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0 ) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}	
+		} 
+		else {
+			p.setValues(position.getRow(), position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			/*
+			 * Cria duas posicoes auxiliares, p e p2. Testa se é possível mover duas casas para frente usando a posicão inicial
+			 * e testa se há alguma peca que possa impedir que o peao se mova duas casas. Por exemplo, no caso em que o peão
+			 * não tenha se movido ainda, mas há um outro peão diretamente na sua frente. 
+			*/
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0 ) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}	
+		}
+		return mat;
+	}
+	
+	
+
+}
