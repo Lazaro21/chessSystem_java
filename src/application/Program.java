03fc9f468745a675dec466746d66315290c7712e
PinkPynchon <olsf21@gmail.com>
2021-03-19 11:21:17 -0300

    Special play promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (Q/R/B/N): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e){
 				System.out.println(e.getMessage());
